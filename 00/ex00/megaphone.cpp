b5c6d4809190a3e8844e510f12971df07af20b59
Astrid Geels <moerastrid@gmail.com>
2023-02-01 19:46:07 +0100

    for loops instead of while loops because it looks more fancy

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: astrid <astrid@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/22 17:41:36 by astrid        #+#    #+#                 */
-/*   Updated: 2023/01/30 13:24:23 by ageels        ########   odam.nl         */
+/*   Updated: 2023/02/01 19:44:55 by ageels        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,17 @@
 
 int main(int argc, char **argv)
 {
-    int i;
-    int j;
+	int j;
 
-    if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    else
-    {
-        i = 1;
-        while (argv[i])
-        {
-            j = 0;
-            while (argv[i][j])
-            {
-                std::cout << (char)toupper(argv[i][j]);
-                j++;
-            }
-            i++;
-        }
-    }
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		return (0);
+	}
+	for (int i = 1; argv[i]; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)toupper(argv[i][j]);
+	}
 	std::cout << "\n";
 }
